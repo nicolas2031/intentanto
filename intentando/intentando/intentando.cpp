8aab131ff4053bc4bb602132855fe0f60013155a
USUARIO <USUARIO@DESKTOP-K6KE95E>
2020-04-13 20:25:08 -0500

    un poco de if

--- intentando/intentando/intentando.cpp
+++ intentando/intentando/intentando.cpp
@@ -1,19 +1,27 @@
-// intentando.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
 
+
+
 int main()
+
+
 {
-    std::cout << "Hello World!\n";
+
+	int x;
+
+
+	std::cin >> x;
+
+	if (x == 20 || x == 31) {
+
+			
+
+		std::cout << "eres muy suertudo" << std::endl;
+	}
+
+	else{
+		std::cout << "no tienes suerte" << std::endl;
+	}
+	
+	return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
